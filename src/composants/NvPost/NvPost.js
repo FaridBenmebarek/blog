281c4178f4a24040e6e5da54b043771132ff897a
Farid Benmebarek <farid.benmebarek@hotmail.fr>
2019-05-30 18:45:25 +0200

    first commit

---
+++ src/composants/NvPost/NvPost.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import './NvPost.css'
+
+class NvPost extends Component {
+
+    state = {
+        title: '',
+        content: '',
+        author: 'Administrateur'
+    }
+
+    postArticle = () => {
+      const nvPost = {
+        title: this.state.title,
+        content: this.state.content,
+        auteur: this.state.author
+      }
+      axios.post('https://jsonplaceholder.typicode.com/posts', {nvPost})
+    }
+
+    render () {
+        return (
+            <div className="NvPost form-group">
+                <h1>Ajouter un Article</h1>
+                <label>Titre</label>
+                <input className="form-control" type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+                <label>Contenu</label>
+                <textarea className="form-control" rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <label>Auteur</label>
+                <select className="form-control" value= {this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                    <option value="administrateur">Administrateur</option>
+                    <option value="redacteur">Rédacteur</option>
+                    <option value="anonyme">Anonyme</option>
+                </select>
+                <button className="btn btn-primary my-3" onClick={this.postArticle}>Ajouter un Article</button>
+            </div>
+        );
+    }
+}
+
+export default NvPost;
---
+++ src/composants/PostComplet/PostComplet.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import './PostComplet.css'
+
+class PostComplet extends Component {
+  state = {
+    loadedPost: null
+  }
+
+  componentDidUpdate () {
+    if (this.props.id) {
+      if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+          .then(res => {
+            this.setState({
+              loadedPost: res.data
+            })
+          })
+      }
+    }
+  }
+
+  supprimePost = () => {
+    axios.delete('https://jsonplaceholder.typicode.com/posts/\' + this.props.id')
+    console.log(`L'article "${this.state.loadedPost.title}" a bien été supprimé !`)
+  }
+
+  render () {
+    let post = <p className="text-center display-4">Choisis un article !</p>
+
+    if (this.props.id) {
+      post = <p className="text-center">Chargement...</p>
+    }
+
+    if (this.state.loadedPost) {
+      post = (
+        <div className="PostComplet">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button className="btn btn-danger my-3" onClick={this.supprimePost}>Supprimer</button>
+          </div>
+        </div>
+
+      )
+    }
+    return post
+  }
+}
+
+export default PostComplet
